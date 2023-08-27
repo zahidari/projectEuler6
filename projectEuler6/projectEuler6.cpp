928a1cee7283ef076a22c88d917290a0ccafe4f6
zahidari <100021451+zahidari@users.noreply.github.com>
2023-08-27 14:22:59 +0200

    Update projectEuler6.cpp
    add desriptions

--- projectEuler6/projectEuler6.cpp
+++ projectEuler6/projectEuler6.cpp
@@ -3,26 +3,22 @@ using namespace std;
 
 int main()
 {
-    int sumThenSquare = 0;
-    int sumOfSquares = 0;
-    int limit = 100;
-    int difference;
+    int sumThenSquare = 0;   // Initialize variable to hold sum of numbers.
+    int sumOfSquares = 0;    // Initialize variable to hold sum of squared numbers.
+    int limit = 100;         // Define the range of numbers from 1 to 100.
+    int difference;          // Variable to store the difference between two sums.
 
-    
-
+    // Loop through numbers from 1 to 100.
     for (int i = 1; i <= limit; i++) {
-        sumThenSquare = sumThenSquare + i;
-        sumOfSquares = sumOfSquares + (i * i);
+        sumThenSquare += i;              // Accumulate sum of numbers.
+        sumOfSquares += (i * i);         // Accumulate sum of squared numbers.
     }
         
-    sumThenSquare = sumThenSquare * sumThenSquare;
-    
+    sumThenSquare = sumThenSquare * sumThenSquare; // Square the sum of numbers.
 
-    //the difference
-    difference = sumThenSquare - sumOfSquares;
+    difference = sumThenSquare - sumOfSquares;     // Calculate the difference.
 
-    cout << difference;
-    return 0;
+    cout << difference;   // Output the difference.
 
+    return 0;             // Indicate successful completion.
 }
-
